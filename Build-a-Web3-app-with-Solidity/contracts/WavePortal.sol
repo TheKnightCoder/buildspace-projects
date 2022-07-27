dcc209f094dca482c23ae5760650431110a1fb2e
Russell Choudhury <russell.choudhury@gmail.com>
2022-07-27 05:36:29 +0100

    cooldown + randomness

--- Build-a-Web3-app-with-Solidity/contracts/WavePortal.sol
+++ Build-a-Web3-app-with-Solidity/contracts/WavePortal.sol
@@ -6,68 +6,80 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
-    /*
-     * A little magic, Google what events are in Solidity!
-     */
     event NewWave(address indexed from, uint256 timestamp, string message);
 
-    /*
-     * I created a struct here named Wave.
-     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
-     */
     struct Wave {
-        address waver; // The address of the user who waved.
-        string message; // The message the user sent.
-        uint256 timestamp; // The timestamp when the user waved.
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    /*
-     * I declare a variable waves that lets me store an array of structs.
-     * This is what lets me hold all the waves anyone ever sends to me!
-     */
     Wave[] waves;
 
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
-        console.log("I AM SMART CONTRACT. POG.");
+        console.log("We have been constructed!");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
-    /*
-     * You'll notice I changed the wave function a little here as well and
-     * now it requires a string called _message. This is the message our user
-     * sends us from the frontend!
-     */
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
-    /*
-     * I added a function getAllWaves which will return the struct array, waves, to us.
-     * This will make it easy to retrieve the waves from our website!
-     */
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        // Optional: Add this line if you want to see the contract print the value!
-        // We'll also print it over in run.js as well.
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
 // WavePortal v1 address:  0xA4D048e86Dc11e8b45Ee5319B9a28C7CE607712B
 // WavePortal v2 address:  0x57b3F1F6D43beCAbF7A6fbd87436c10AA42026D9
+// WavePortal v3 address:  0xB6703BA2872d1e5d0C256b73433dfCDb140BB9F3
+// WavePortal v4 address:  0x84827ed4098dC1d3feb8A1919D9C75D84703acBe
