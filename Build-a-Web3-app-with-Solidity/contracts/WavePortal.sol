194a919cc0bbb506f458299fd20d7c04c1964295
Russell Choudhury <russell.choudhury@gmail.com>
2022-07-27 05:06:35 +0100

    task 1

---
+++ Build-a-Web3-app-with-Solidity/contracts/WavePortal.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
+
+// WavePortal address:  0xA4D048e86Dc11e8b45Ee5319B9a28C7CE607712B
