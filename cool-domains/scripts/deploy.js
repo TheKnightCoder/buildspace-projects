f8b29906c78175856cd20aeeb59114ed4ec12179
Russell Choudhury <russell.choudhury@gmail.com>
2022-09-13 17:46:34 +0100

    reactapp

--- cool-domains/scripts/deploy.js
+++ cool-domains/scripts/deploy.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy("ninja");
+  const domainContract = await domainContractFactory.deploy("frog");
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
@@ -8,11 +8,11 @@ const main = async () => {
   // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
   let txn = await domainContract.register("banana", { value: hre.ethers.utils.parseEther('0.1') });
   await txn.wait();
-  console.log("Minted domain banana.ninja");
+  console.log("Minted domain banana.frog");
 
-  txn = await domainContract.setRecord("banana", "Am I a banana or a ninja??");
+  txn = await domainContract.setRecord("banana", "Am I a banana or a frog??");
   await txn.wait();
-  console.log("Set record for banana.ninja");
+  console.log("Set record for banana.frog");
 
   const address = await domainContract.getAddress("banana");
   console.log("Owner of domain banana:", address);
